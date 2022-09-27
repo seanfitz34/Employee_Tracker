1fc7a67a18f386e7c0677aee08bb62526eb435ed
seanfitz34 <seanwilliamfitzgerald@gmail.com>
2022-09-26 18:13:22 -0700

    updating

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,24 @@
+-- create data-base
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db; 
 
 
-
+-- create department table
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
-
+-- create role table
 CREATE TABLE role (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL NOT NULL,
     department_id INTEGER, 
     INDEX dep_ind (department_id),
-    CONSTRAINT fk_department 
-    FOREIGN KEY (department_id) 
-    REFERENCES department(id) 
-    ON DELETE SET NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
-
+-- create employee table
 CREATE TABLE employee (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
